8c89e537a1b633465061ebf2eec33cbc0f68d016
kingdaro <19603573+kingdaro@users.noreply.github.com>
2020-08-08 19:43:52 -0500

    fix type on useEffectRef
    RefObject makes the value nullable

--- react/useEffectRef.ts
+++ react/useEffectRef.ts
@@ -1,13 +1,13 @@
-import { RefObject, useEffect, useRef } from "react"
+import { useEffect, useRef } from "react"
 
 /**
  * Use this when you want to read a value in useEffect/useCallback
  * without it being a dependency
  */
-export function useEffectRef<T>(value: T) {
+export function useEffectRef<T>(value: T): { readonly current: T } {
   const ref = useRef(value)
   useEffect(() => {
     ref.current = value
   })
-  return ref as RefObject<T>
+  return ref
 }
